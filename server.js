fa40d97605a475d599b27ed74383eaea9b5fb870
adil tahiri <rajaadil1995@gmail.com>
2022-09-26 01:38:22 +0100

    add home route

--- server.js
+++ server.js
@@ -7,42 +7,45 @@ const MongoStore = require("connect-mongo");
 const flash = require("express-flash");
 const logger = require("morgan");
 const connectDB = require("./config/db");
-// const indexRouter = require("./routes/index");
-// const homeRouter = require("./routes/home");
-// const userRouter = require("./routes/user");
+const homeRouter = require("./routes/home");
+const indexRouter = require("./routes/index");
+const userRouter = require("./routes/user");
 
 require("dotenv").config({ path: "./config/.env" });
 
 // Passport config
-//require('./config/passport')(passport)
+require("./config/passport")(passport);
 
 connectDB();
 
+app.use(express.json());
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 app.use(logger("dev"));
+app.set("trust proxy", 1);
 // Sessions
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
+    cookie: { secure: false, httpOnly: true, sameSite: true },
     // store: new MongoStore({ mongooseConnection: mongoose.connection }),
     store: MongoStore.create({ mongoUrl: process.env.CONNECT_DB }),
   })
 );
 
+// app.use(cookies());
 // Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(flash());
 
-// app.use("/", indexRouter);
-// app.use("/menu", homeRouter);
-// app.use("/user", userRouter);
+app.use("/", indexRouter);
+app.use("/home", homeRouter);
+app.use("/user", userRouter);
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running, you better catch it!");
