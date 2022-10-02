797ac19cdb8b25cd7b5ab99ef78066d632b69ddf
adil tahiri <rajaadil1995@gmail.com>
2022-10-03 00:20:35 +0100

    fix delete routes

--- routes/home.js
+++ routes/home.js
@@ -8,7 +8,7 @@ router.get("/", ensureAuth, homeController.getHome);
 router.get("/new", ensureAuth, homeController.getAddTask);
 router.get("/editTask/:id", ensureAuth, homeController.getEditTask);
 router.get("/spend/:id", ensureAuth, homeController.getSpends);
-router.delete("//deletePost/:id", homeController.deleteSpends);
+router.delete("/deletePost/:id", homeController.deleteSpends);
 router.post("/newTask", homeController.postAddTask);
 router.put("/edit/:id", homeController.editTask);
 
