9302c084b8be4b3d76a30b13483291e3a06667aa
adil tahiri <rajaadil1995@gmail.com>
2022-10-07 23:53:22 +0100

    add week route

--- routes/home.js
+++ routes/home.js
@@ -8,8 +8,12 @@ router.get("/", ensureAuth, homeController.getHome);
 router.get("/new", ensureAuth, homeController.getAddTask);
 router.get("/editTask/:id", ensureAuth, homeController.getEditTask);
 router.get("/spend/:id", ensureAuth, homeController.getSpends);
+router.get("/:year", ensureAuth, homeController.getYear);
+router.get("/day", ensureAuth, homeController.getDay);
+router.get("/week/:week", ensureAuth, homeController.getWeek);
+router.get("/month/:month", ensureAuth, homeController.getMonth);
 router.delete("/deletePost/:id", homeController.deleteSpends);
-router.post("/newTask", homeController.postAddTask);
+router.post("/newTask", ensureAuth, homeController.postAddTask);
 router.put("/edit/:id", homeController.editTask);
 
 module.exports = router;
