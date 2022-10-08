637e1251ab823b9a9ee8531815d554d2af8b38aa
adil tahiri <rajaadil1995@gmail.com>
2022-10-08 18:57:54 +0100

    add profile count data

--- controllers/profile.js
+++ controllers/profile.js
@@ -3,13 +3,26 @@ const Tasks = require("../models/Tasks");
 module.exports = {
   getProfile: async (req, res) => {
     try {
-      res.render("profile.ejs", { user: req.user, error: "" });
-      const tasks = await Tasks.find().sort({ createdAt: "desc" }).lean();
-      // const user = await User.find();
-      res.render("profile.ejs", { tasks: tasks, user: req.user });
+      const tasks = await Tasks.find();
+      const result = await Tasks.count({ user: req.user.id });
+      res.render("profile.ejs", {
+        tasks: tasks,
+        user: req.user,
+        error: "",
+        result: result,
+      });
     } catch (err) {
       console.log(err);
       res.render("error404.ejs");
     }
+  },
+  getCount: async (req, res) => {
+    Tasks.count({}, function (err, result) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.render("profile.ejs", { result: result });
+      }
+    });
   },
 };
