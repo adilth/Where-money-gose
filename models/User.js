9cd3bf05133fa08813f7f8e525dc71058555d98b
adil tahiri <rajaadil1995@gmail.com>
2022-10-22 22:30:53 +0100

    add require to all fields

--- models/User.js
+++ models/User.js
@@ -2,9 +2,9 @@ const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  userName: { type: String, unique: true },
-  email: { type: String, unique: true },
-  password: String,
+  userName: { type: String, unique: true, require: true },
+  email: { type: String, unique: true, require: true },
+  password: { type: String, require: true },
 });
 
 // Password hash middleware.
@@ -38,5 +38,44 @@ UserSchema.methods.comparePassword = function comparePassword(
     cb(err, isMatch);
   });
 };
+// UserSchema.methods.changePassword = function (oldPassword, newPassword, cb) {
+//   if (!oldPassword || !newPassword) {
+//     return cb(
+//       new errors.MissingPasswordError(
+//         options.errorMessages.MissingPasswordError
+//       )
+//     );
+//   }
+
+//   var self = this;
+
+//   this.authenticate(oldPassword, function (err, authenticated) {
+//     if (err) {
+//       return cb(err);
+//     }
+
+//     if (!authenticated) {
+//       return cb(
+//         new errors.IncorrectPasswordError(
+//           options.errorMessages.IncorrectPasswordError
+//         )
+//       );
+//     }
+
+//     self.setPassword(newPassword, function (setPasswordErr, user) {
+//       if (setPasswordErr) {
+//         return cb(setPasswordErr);
+//       }
+
+//       self.save(function (saveErr) {
+//         if (saveErr) {
+//           return cb(saveErr);
+//         }
+
+//         cb(null, user);
+//       });
+//     });
+//   });
+// };
 
 module.exports = mongoose.model("User", UserSchema);
