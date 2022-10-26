e296dbcc3359110b46e2ee70825692de1e7e235f
adil tahiri <rajaadil1995@gmail.com>
2022-10-27 00:59:46 +0100

    add search controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,52 @@
 const User = require("../models/User");
+const Tasks = require("../models/Tasks");
+const mongoose = require("mongoose");
 module.exports = {
   getIndex: async (req, res) => {
     const user = await User.find();
     res.render("index.ejs", { user: user });
+  },
+  getSearch: async (req, res) => {
+    let q = req.body.searchInput;
+    console.log(q);
+    let taskData = null;
+    let qry = { $or: [{ task: { $regex: q } }] };
+    console.log(qry);
+    try {
+      if (q != null) {
+        let taskResult = await Tasks.find(qry);
+        taskData = taskResult;
+        console.log(taskData);
+      } else {
+        q = "search";
+        let taskResult = await Tasks.find({});
+        taskData = taskResult;
+      }
+      const total = await Tasks.aggregate([
+        {
+          $match: {
+            user: mongoose.Types.ObjectId(req.user.id),
+            task: q,
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            count: {
+              $sum: "$spend",
+            },
+          },
+        },
+      ]);
+      res.render("dashboard", {
+        title: "task Tracker",
+        tasks: taskData,
+        search: q,
+        user: req.user,
+        total: total,
+      });
+    } catch (e) {
+      console.error(e);
+    }
   },
 };
