5883354f173bfbcaf36b90bc2c9fc07503be1fc5
adil tahiri <rajaadil1995@gmail.com>
2022-11-19 23:00:35 +0100

    add contact us method get and post

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,9 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now
 router.get("/", indexController.getIndex);
+router.get("/contact", ensureAuth, indexController.getContactUs);
 router.post("/q", ensureAuth, indexController.getSearch);
+router.post("/contact", ensureAuth, indexController.contactUs);
 // router.get("dashboard", ensureAuth, postsController.getFeed);
 
 module.exports = router;
