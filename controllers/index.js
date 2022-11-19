ef2a1a0412fe125612e67a24fa2b05d6ddd3e659
adil tahiri <rajaadil1995@gmail.com>
2022-11-19 23:01:06 +0100

    add contact us method get and post

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const Tasks = require("../models/Tasks");
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
 module.exports = {
   getIndex: async (req, res) => {
     const user = await User.find();
@@ -60,6 +61,46 @@ module.exports = {
       });
     } catch (e) {
       console.error(e);
+    }
+  },
+  getContactUs: async (req, res) => {
+    try {
+      const user = await Tasks.find({ user: req.user.id });
+      res.render("contact.ejs", { user: req.user, msg: "", search: null });
+    } catch (e) {
+      console.error(err);
+      res.render("error500.ejs");
+    }
+  },
+  contactUs: async (req, res) => {
+    try {
+      const { name, UEmail, subject, message } = req.body;
+      const user = await Tasks.find({ user: req.user.id });
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: process.env.BUSINESS_EMAIL,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+      });
+      const data = {
+        from: UEmail,
+        to: process.env.BUSINESS_EMAIL,
+        subject: subject,
+        html: ` <p>email from ${name} and his ${UEmail} send from spend money  </p>
+        <p> ${message} </p>
+        `,
+      };
+      console.log(data);
+      // let emailTransporter = await createTransporter();
+      await transporter.sendMail(data);
+      req.flash("errors", {
+        msg: "your email has successfully send message",
+      });
+      res.render("contact", { user: req.user, search: null });
+    } catch (err) {
+      console.error(err);
+      res.render("error500.ejs");
     }
   },
 };
