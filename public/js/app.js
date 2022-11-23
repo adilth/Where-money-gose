3665ff589cce907813b570031500ace232a2af26
adil tahiri <rajaadil1995@gmail.com>
2022-11-23 22:35:36 +0100

    add some overlay to nav-menu

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 let subMenu = document.querySelector(".profile-link");
 let imgPro = document.getElementById("img-pro");
-
+const overlay = document.querySelector(".overlay");
 function toggleMenu() {
   subMenu.classList.remove("hidden");
   subMenu.classList.toggle("open-menu");
@@ -11,23 +11,37 @@ if (imgPro) {
 
 const primaryNav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
+function togglePrimaryNav() {
+  const visibility = primaryNav.getAttribute("data-visible");
+  if (visibility === "false") {
+    overlay.classList.remove("hidden");
+    primaryNav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+  } else {
+    overlay.classList.add("hidden");
+    primaryNav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+  }
+}
 
 if (navToggle) {
-  navToggle.addEventListener("click", () => {
+  navToggle.addEventListener("click", togglePrimaryNav);
+  overlay.addEventListener("click", () => {
     const visibility = primaryNav.getAttribute("data-visible");
     if (visibility === "false") {
-      primaryNav.setAttribute("data-visible", true);
-      navToggle.setAttribute("aria-expanded", true);
+      overlay.classList.remove("hidden");
     } else {
+      overlay.classList.add("hidden");
       primaryNav.setAttribute("data-visible", false);
       navToggle.setAttribute("aria-expanded", false);
     }
   });
 }
 const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const openModalBtn = document.querySelector(".btn-open");
-const closeModalBtn = document.querySelector(".btn-close");
+
+const openModalBtn = document.querySelectorAll(".btn-open");
+const openModalId = document.querySelector(".btn-open");
+const closeModalBtn = document.querySelectorAll(".btn-close");
 
 const openModal = function () {
   modal.classList.remove("hidden");
@@ -38,9 +52,9 @@ const closeModal = function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
 };
-if (openModalBtn) {
-  openModalBtn.addEventListener("click", openModal);
-  closeModalBtn.addEventListener("click", closeModal);
+if (openModalId) {
+  openModalBtn.forEach((el) => el.addEventListener("click", openModal));
+  closeModalBtn.forEach((el) => el.addEventListener("click", closeModal));
   overlay.addEventListener("click", closeModal);
 }
 document.addEventListener("keydown", function (e) {
