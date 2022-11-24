b19752056f74d9d78b4fa5757e4ff03ad5925a68
adil tahiri <rajaadil1995@gmail.com>
2022-11-24 23:23:09 +0100

    add url if not found

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// app.use(helmet());
 app.use(helmet({ contentSecurityPolicy: false }));
 // app.use(express.static(path.join(__dirname, "./public")));
 
@@ -65,11 +66,13 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(flash());
-
 app.use("/", indexRouter);
 app.use("/home", homeRouter);
 app.use("/user", userRouter);
 app.use("/profile", profileRouter);
+app.use((req, res, next) => {
+  res.status(404).render("error404.ejs");
+});
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running, you better catch it!");
