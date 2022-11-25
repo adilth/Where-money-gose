b77bc34d1e98f36222d16ad5b7eeb302e747d90d
adil tahiri <rajaadil1995@gmail.com>
2022-11-25 19:18:42 +0100

    remove google api package

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,36 +5,7 @@ const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
-const mongoose = require("mongoose");
 const OAuth2 = google.auth.OAuth2;
-
-const createTransporter = async () => {
-  const oauth2Client = new OAuth2(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    "https://developers.google.com/oauthplayground"
-  );
-
-  oauth2Client.setCredentials({
-    refresh_token: process.env.REFRESH_TOKEN,
-  });
-
-  const accessToken = await oauth2Client.getAccessToken();
-
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      type: "OAuth2",
-      user: process.env.BUSINESS_EMAIL,
-      accessToken,
-      clientId: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-      refreshToken: process.env.REFRESH_TOKEN,
-    },
-  });
-
-  return transporter;
-};
 
 module.exports = {
   getLogin: async (req, res) => {
