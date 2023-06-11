7d00025e9d3ebde5010a9fbb5d60bc299305b5e1
adil tahiri <rajaadil1995@gmail.com>
2023-06-11 17:46:12 +0100

    change some route params

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 //Main Routes - simplified for now
 router.get("/", indexController.getIndex);
 router.get("/contact", ensureAuth, indexController.getContactUs);
-router.post("/q", ensureAuth, indexController.getSearch);
+router.post("/home/q", ensureAuth, indexController.getSearch);
 router.post("/contact", ensureAuth, indexController.contactUs);
 // router.get("dashboard", ensureAuth, postsController.getFeed);
 
