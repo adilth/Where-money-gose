66452411174f856e69b297a2aa317df0c6131695
adil tahiri <rajaadil1995@gmail.com>
2023-06-11 17:50:35 +0100

    and a trims to in models for database to not containes spaces

--- models/User.js
+++ models/User.js
@@ -1,12 +1,17 @@
 const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  userName: { type: String, unique: true, require: true },
-  email: { type: String, unique: true, require: true },
-  password: { type: String, require: true },
-  resetPasswordToken: String,
-});
+const UserSchema = new mongoose.Schema(
+  {
+    userName: { type: String, trim: true, unique: true, require: true },
+    email: { type: String, trim: true, unique: true, require: true },
+    password: { type: String, trim: true, require: true },
+    resetPasswordToken: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 // Password hash middleware.
 
@@ -39,44 +44,4 @@ UserSchema.methods.comparePassword = function comparePassword(
     cb(err, isMatch);
   });
 };
-// UserSchema.methods.changePassword = function (oldPassword, newPassword, cb) {
-//   if (!oldPassword || !newPassword) {
-//     return cb(
-//       new errors.MissingPasswordError(
-//         options.errorMessages.MissingPasswordError
-//       )
-//     );
-//   }
-
-//   var self = this;
-
-//   this.authenticate(oldPassword, function (err, authenticated) {
-//     if (err) {
-//       return cb(err);
-//     }
-
-//     if (!authenticated) {
-//       return cb(
-//         new errors.IncorrectPasswordError(
-//           options.errorMessages.IncorrectPasswordError
-//         )
-//       );
-//     }
-
-//     self.setPassword(newPassword, function (setPasswordErr, user) {
-//       if (setPasswordErr) {
-//         return cb(setPasswordErr);
-//       }
-
-//       self.save(function (saveErr) {
-//         if (saveErr) {
-//           return cb(saveErr);
-//         }
-
-//         cb(null, user);
-//       });
-//     });
-//   });
-// };
-
 module.exports = mongoose.model("User", UserSchema);
